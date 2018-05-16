8cd5f232b81a57305128d99fd25148861593fe85
Andy Carter <andy1704@gmail.com>
2018-05-16 19:14:12 +0100

    Update documentation and license

--- src/arc_social_share.php
+++ src/arc_social_share.php
@@ -1,8 +1,8 @@
 <?php
 $plugin['name'] = 'arc_social_share';
-$plugin['version'] = '1.4.0';
+$plugin['version'] = '1.5.0';
 $plugin['author'] = 'Andy Carter';
-$plugin['author_uri'] = 'http://andy-carter.com/';
+$plugin['author_uri'] = 'https://andy-carter.com/';
 $plugin['description'] = 'Social media share links';
 $plugin['order'] = '5';
 $plugin['type'] = '0';
@@ -339,10 +339,6 @@ h1. arc_social_share
 
 Easily add links for sharing content with numerous social networks.
 
-h2. Installation
-
-To install go to 'Plugins' under 'Admin' and paste the plugin code into the 'Install plugin' box, 'upload' and then 'install'. You will then need to activate the plugin.
-
 h2. The Tags
 
 All tags have the following attributes:-
@@ -430,19 +426,11 @@ bc. <txp:arc_social_share_twitter class='bookmarklet' />
 
 h2. Author
 
-"Andy Carter":http://andy-carter.com. For other Textpattern plugins by me visit my "Plugins page":http://andy-carter.com/txp.
+"Andy Carter":https://andy-carter.com.
 
 h2. License
 
-The MIT License (MIT)
-
-Copyright (c) 2015 Andy Carter
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"License":https://github.com/drmonkeyninja/arc_social_share/blob/master/LICENSE
 
 # --- END PLUGIN HELP ---
 -->
